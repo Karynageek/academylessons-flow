cddba882eb8d73184a456f5f9f5ebdbbaa369310
Karyna Chelpan <cheplan15@gmail.com>
2020-07-09 10:56:25 +0300

    [add] driver

--- src/main/java/org/academy/utils/web/LocalWebDriverManager.java
+++ src/main/java/org/academy/utils/web/LocalWebDriverManager.java
@@ -23,36 +23,36 @@ public class LocalWebDriverManager {
     public static WebDriver getWebDriver(String browser) {
         switch (browser) {
             default:
-            //case "chrome":
-              //  ChromeOptions options = new ChromeOptions();
-                //String os = System.getProperty("os.name").toLowerCase().substring(0, 3);
-                //String chromeBinary = "src/main/resources/chromedriver" + (os.equals("win") ? ".exe" : "");
-                //System.setProperty("webdriver.chrome.driver", chromeBinary);
-                //WebDriver chromeWebDriver = new ChromeDriver(options);
+            case "chrome":
+                ChromeOptions options = new ChromeOptions();
+                String os = System.getProperty("os.name").toLowerCase().substring(0, 3);
+                String chromeBinary = "src/main/resources/chromedriver" + (os.equals("win") ? ".exe" : "");
+                System.setProperty("webdriver.chrome.driver", chromeBinary);
+                WebDriver chromeWebDriver = new ChromeDriver(options);
 
-              //  chromeWebDriver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
+                chromeWebDriver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
 
-              //  chromeWebDriver.manage().window().maximize();
-               // return chromeWebDriver;
-          //  case "chrome_remote_selenoid":
-          //      DesiredCapabilities capabilities = new DesiredCapabilities();
-          //      capabilities.setBrowserName("chrome");
-          //      capabilities.setVersion("83.0");
-          //      capabilities.setCapability("enableVNC", true);
-          //      capabilities.setCapability("enableVideo", false);
+                chromeWebDriver.manage().window().maximize();
+                return chromeWebDriver;
+            case "chrome_remote_selenoid":
+                DesiredCapabilities capabilities = new DesiredCapabilities();
+                capabilities.setBrowserName("chrome");
+                capabilities.setVersion("83.0");
+                capabilities.setCapability("enableVNC", true);
+                capabilities.setCapability("enableVideo", false);
 
-           //     RemoteWebDriver driver = null;
-           //     try {
-          //          driver = new RemoteWebDriver(
-           //                 URI.create("http://localhost:4444/wd/hub").toURL(),
-           //                 capabilities
-          //        );
-          //      } catch (MalformedURLException e) {
-          //          e.printStackTrace();
-          //      }
-          //      driver.manage().timeouts().implicitlyWait(40000, TimeUnit.MILLISECONDS);
-          //      driver.manage().window().maximize();
-            //    return driver;
+                RemoteWebDriver driver = null;
+                try {
+                    driver = new RemoteWebDriver(
+                          URI.create("http://localhost:4444/wd/hub").toURL(),
+                           capabilities
+                  );
+                } catch (MalformedURLException e) {
+                   e.printStackTrace();
+                }
+                driver.manage().timeouts().implicitlyWait(40000, TimeUnit.MILLISECONDS);
+                driver.manage().window().maximize();
+                return driver;
             case "chrome_remote_selenium":
                 DesiredCapabilities caps = DesiredCapabilities.chrome();
                 RemoteWebDriver wdriver = null;
